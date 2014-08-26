c37c55901868f90b0804c6eb912d71facd437228
wulin <125906088@qq.com>
2014-08-26 18:51:06 +0800

    svd code commit

--- datamining/src/main/java/org/project/common/document/DocumentUtils.java
+++ datamining/src/main/java/org/project/common/document/DocumentUtils.java
@@ -1,14 +1,17 @@
 package org.project.common.document;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.project.common.distance.CosineDistance;
 import org.project.common.distance.IDistance;
+
+import Jama.Matrix;
+import Jama.SingularValueDecomposition;
 
 public class DocumentUtils {
 	
@@ -84,8 +87,9 @@ public class DocumentUtils {
 	}
 	
 	/**
-	 * 计算相似度
+	 * 计算文档间的相似度
 	 * @param documents
+	 * @param idistance
 	 */
 	public static void calculateSimilarity(List<Document> documents, IDistance idistance) {
 		for (Document document : documents) {
@@ -100,12 +104,95 @@ public class DocumentUtils {
 			System.out.println("doc " + document.getName() + " calculate similar finish");
 		}
 	}
-
+	
+	/**
+	 * 转化为矩阵 行为文档 列为词
+	 * @param documents
+	 * @return
+	 */
+	public static double[][] convertToDocTermMatrix(List<Document> documents) {
+		Collection<String> allWords = DocumentHelper.wordsInDocs(documents, true);
+		int docSize = documents.size(), allWordsSize = allWords.size();
+		double[][] matrix = new double[docSize][allWordsSize];
+		for (int i = 0; i < docSize; i++) {
+			Map<String, Double> tfidfWords = documents.get(i).getTfidfWords();
+			int j = 0;
+			for (String word : allWords) {
+				if (tfidfWords.containsKey(word)) {
+					matrix[i][j] = tfidfWords.get(word).doubleValue();
+				} else {
+					matrix[i][j] = 0;
+				}
+				j++;
+			}
+		}
+		return matrix;
+	}
+	
+	/**
+	 * 转化为矩阵 行为词 列为文档
+	 * @param documents
+	 * @return
+	 */
+	public static double[][] convertToTermDocMatrix(List<Document> documents) {
+		Collection<String> allWords = DocumentHelper.wordsInDocs(documents, true);
+		int docSize = documents.size(), allWordsSize = allWords.size();
+		double[][] matrix = new double[allWordsSize][docSize];
+		for (int i = 0; i < docSize; i++) {
+			Map<String, Double> tfidfWords = documents.get(i).getTfidfWords();
+			int j = 0;
+			for (String word : allWords) {
+				if (tfidfWords.containsKey(word)) {
+					matrix[j][i] = tfidfWords.get(word).doubleValue();
+				} else {
+					matrix[j][i] = 0;
+				}
+				j++;
+			}
+		}
+		return matrix;
+	}
+	
+	/**
+	 * 计算相似矩阵
+	 * A * A' = D * S * S' * D'
+	 * A' * A = D * S' * S * D'
+	 * 比较两个术语  做"正向"乘法；第i行第j列表明了术语i和j的相似程度
+	 * 比较两个文件做"逆向"乘法；第i行第j列表明了文件i和j的相似程度
+	 * svd函数只适合行数大于列数的矩阵，如果行数小于列数，可对其转置矩阵做SVD分解 
+	 * @param matrix
+	 * @param dimension 维数
+	 * @return
+	 */
+	public static double[][] calculateSimilarityMatrix(double[][] matrix, int dimension) {
+		Matrix sMatrix = new Matrix(matrix);  
+		boolean flag = matrix.length >= matrix[0].length ? true : false;
+        SingularValueDecomposition svd = flag ? sMatrix.svd() : sMatrix.transpose().svd();  
+        Matrix D = svd.getU();  
+        Matrix S = svd.getS();  
+        for(int i = dimension; i < S.getRowDimension(); i++){//降到dimension维  
+            S.set(i, i, 0);  
+        }  
+        return flag ? D.times(S.times(S.transpose().times(D.transpose()))).getArray() 
+        		: D.times(S.transpose().times(S.times(D.transpose()))).getArray();  
+	}
+	
 	public static void main(String[] args) throws Exception {
-		String path = DocumentUtils.class.getClassLoader().getResource("测试").toURI().getPath();
-		DocumentSet dataSet = DocumentLoader.loadDocumentSet(path);
-		calculateTFIDF_0(dataSet.getDocuments());
-		calculateSimilarity(dataSet.getDocuments(), new CosineDistance());
+		String path = DocumentUtils.class.getClassLoader().getResource("微测").toURI().getPath();
+		DocumentSet documentSet = DocumentLoader.loadDocumentSet(path);
+		calculateTFIDF_0(documentSet.getDocuments());
+//		calculateSimilarity(documentSet.getDocuments(), new CosineDistance());
+//		double[][] tdmatrix = convertToDocTermMatrix(documentSet.getDocuments());
+		double[][] tdmatrix = convertToTermDocMatrix(documentSet.getDocuments());
+		System.out.println("matrix row: " + tdmatrix.length + " col: " + tdmatrix[0].length);
+		double[][] matrix = calculateSimilarityMatrix(tdmatrix, 100);
+		System.out.println("matrix row: " + matrix.length + " col: " + matrix[0].length);
+//		for (int i = 0, row = matrix.length; i < row; i++) {
+//			for (int j = 0, col = matrix[0].length; j < col; j++) {
+//				System.out.print(matrix[i][j] + ",");
+//			}
+//			System.out.println();
+//		}
 	}
 	
 }
---
+++ datamining/src/test/java/org/project/common/matrix/MatrixTest.java
@@ -0,0 +1,37 @@
+package org.project.common.matrix;
+
+import Jama.Matrix;
+import Jama.SingularValueDecomposition;
+
+public class MatrixTest {
+
+	public static void main(String[] args) throws Exception {
+		int M = 8, N = 5;
+		Matrix B = Matrix.random(5, 3);
+		Matrix A = Matrix.random(M, N).times(B).times(B.transpose());
+		System.out.print("A = ");
+		A.print(9, 6);
+
+		// compute the singular vallue decomposition
+		System.out.println("A = U S V^T");
+		System.out.println();
+		SingularValueDecomposition s = A.svd();
+		System.out.print("U = ");
+		Matrix U = s.getU();
+		U.print(9, 6);
+		System.out.print("Sigma = ");
+		Matrix S = s.getS();
+		S.print(9, 6);
+		System.out.print("V = ");
+		Matrix V = s.getV();
+		V.print(9, 6);
+		System.out.println("rank = " + s.rank());
+		System.out.println("condition number = " + s.cond());
+		System.out.println("2-norm = " + s.norm2());
+
+		// print out singular values
+		System.out.print("singular values = ");
+		Matrix svalues = new Matrix(s.getSingularValues(), 1);
+		svalues.print(9, 6);
+	}
+}
