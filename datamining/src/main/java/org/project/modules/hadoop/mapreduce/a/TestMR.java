39223c2273ab4400a1b785b61c26d04350133fd2
wulin <wulin.198804@gmail.com>
2014-12-12 00:24:20 +0800

    code modify commit

--- datamining/src/main/java/org/project/modules/hadoop/mapreduce/a/TestMR.java
+++ datamining/src/main/java/org/project/modules/hadoop/mapreduce/a/TestMR.java
@@ -1,17 +1,16 @@
 package org.project.modules.hadoop.mapreduce.a;
 
-import java.io.DataInput;
-import java.io.DataOutput;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -22,18 +21,18 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class TestMR {
-	
+
 	private static void configureJob(Job job) {
 		job.setJarByClass(TestMR.class);
-		
+
 		job.setMapperClass(TestMapper.class);
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(PWritable.class);
+		job.setMapOutputValueClass(Text.class);
 
 		job.setReducerClass(TestReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
-		
+
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 	}
@@ -41,112 +40,142 @@ public class TestMR {
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration();
 		try {
-			String[] inputArgs = new GenericOptionsParser(
-						configuration, args).getRemainingArgs();
+			String[] inputArgs = new GenericOptionsParser(configuration, args)
+					.getRemainingArgs();
 			if (inputArgs.length != 2) {
-				System.out.println("error, please input two path. input and output");
+				System.out
+						.println("error, please input two path. input and output");
 				System.exit(2);
 			}
 			Job job = Job.getInstance(configuration, "TestMR");
-			
+
 			FileInputFormat.addInputPath(job, new Path(inputArgs[0]));
 			FileOutputFormat.setOutputPath(job, new Path(inputArgs[1]));
-			
+
 			configureJob(job);
-			
+
 			System.out.println(job.waitForCompletion(true) ? 0 : 1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
 
-class TestMapper extends Mapper<LongWritable, Text, Text, PWritable> {
+
+class TestMapper extends Mapper<LongWritable, Text, Text, Text> {
+	
+	private static Text text1 = new Text();
+
+	private Text text2 = new Text();
+	
+	private static int i = 0;
+	
+	private int j = 0;
+	
+	private A a2 = new A("no static");
+	private static A a1 = new A("static");
+	
 	
 	@Override
 	protected void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		String line = value.toString();
-		String[] datas = line.split(" ");
-		double x = Double.parseDouble(datas[0]);
-		double y = Double.parseDouble(datas[1]);
-		context.write(new Text(datas[2]), new PWritable(x, y, datas[2]));
+		text1 = value;
+		text2 = value;
+		i++;
+		j++;
+		System.out.println("map: " + value + " text1: " + text1);
+		System.out.println("map: " + value + " text2: " + text2);
+		System.out.println("map i: " + i);
+		System.out.println("map j: " + j);
+		a1.setS(value.toString());
+		a2.setS(value.toString());
+		System.out.println(getTitle("http://news.163.com/14/1211/18/AD746G3F00014JB5.html"));
+	}
+	
+	@Override
+	protected void cleanup(Context context) throws IOException, InterruptedException {
+		System.out.println("cleanup text1: " + text1);
+		System.out.println("cleanup text2: " + text2);
+		System.out.println("cleanup i: " + i);
+		System.out.println("cleanup j: " + j);
+		System.out.println("cleanup a1: " + a1.getS());
+		System.out.println("cleanup a2: " + a2.getS());
+	}
+	
+	
+	
+	public String getTitle(String URL) {
+		try {
+			URL url = new URL(URL);
+			URLConnection urlcon = url.openConnection();
+			InputStream is = urlcon.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "gbk");
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = new BufferedReader(isr);
+			String tmps = null;
+			int start = -1;
+			int end = -1;
+			int startz = -1;
+			while ((tmps = br.readLine()) != null) {
+				if (startz >= 0) {
+					sb.append(tmps);
+				} else {
+					// 开始找title
+					start = tmps.indexOf("<title>");
+					end = tmps.indexOf("/title>");
+					// System.out.println(tmps);
+					if (start >= 0) {
+						// 找到
+						if (end >= 0) {
+							return tmps.substring(start + 7, end - 1);// 防止乱码
+						}
+						sb.append(tmps);
+						startz = start;
+						continue;
+					}// 没找到
+					continue;
+				}
+				// 找 /title
+				end = sb.indexOf("/title>");
+				if (end >= 0) {// 找到
+					startz = sb.indexOf("<title>") + 7;
+					return sb.substring(startz, end - 1);
+				}
+				// 没找到
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return "";
 	}
 }
 
-class TestReducer extends Reducer<Text, PWritable, Text, Text> {
+class TestReducer extends Reducer<Text, Text, Text, Text> {
 	
 	@Override
-	protected void reduce(Text key, Iterable<PWritable> values, Context context)
+	protected void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
-		System.out.println(key);
-		List<PWritable> ps = new ArrayList<PWritable>();
-		for (PWritable value : values) {
-			System.out.println("v: " + value.getX()+"-"+value.getY()+"-"+value.getCategory());
-			ps.add(value);
-		}
-		for (PWritable value : ps) {
-			System.out.println("p: " + value.getX()+"-"+value.getY()+"-"+value.getCategory());
-		}
+//		System.out.println(getTitle("http://news.163.com/14/1211/08/AD61455P00014AEE.html"));
 	}
 }
 
-class PWritable implements Writable {
+class A {
 	
-	private DoubleWritable x = null;
+	private String t = null;
 	
-	private DoubleWritable y = null;
-	
-	private Text category = null;
-	
-	public PWritable() {
-		this.x = new DoubleWritable();
-		this.y = new DoubleWritable();
-		this.category = new Text();
+	public A(String s) {
+		System.out.println("construct A: " + s);
 	}
 	
-	public PWritable(double x, double y, String category) {
-		this.x = new DoubleWritable(x);
-		this.y = new DoubleWritable(y);
-		this.category = new Text(category);
+	public void setS(String t) {
+		System.out.println("t: " + t);
+		this.t = t;
 	}
-
-	public DoubleWritable getX() {
-		return x;
+	
+	public String getS() {
+		return t;
 	}
-
-	public void setX(DoubleWritable x) {
-		this.x = x;
-	}
-
-	public DoubleWritable getY() {
-		return y;
-	}
-
-	public void setY(DoubleWritable y) {
-		this.y = y;
-	}
-
-	public Text getCategory() {
-		return category;
-	}
-
-	public void setCategory(Text category) {
-		this.category = category;
-	}
-
-	@Override
-	public void readFields(DataInput in) throws IOException {
-		x.readFields(in);
-		y.readFields(in);
-		category.readFields(in);
-	}
-
-	@Override
-	public void write(DataOutput out) throws IOException {
-		x.write(out);
-		y.write(out);
-		category.write(out);
-	}
-
 }
--- datamining/src/test/java/org/project/modules/hadoop/FileOperationTest.java
+++ datamining/src/test/java/org/project/modules/hadoop/FileOperationTest.java
@@ -1,5 +1,61 @@
 package org.project.modules.hadoop;
 
-public class FileOperationTest {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
+public class FileOperationTest {
+	
+	public static String getTitle(String URL) {
+		try {
+			URL url = new URL(URL);
+			URLConnection urlcon = url.openConnection();
+			InputStream is = urlcon.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "gbk");
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = new BufferedReader(isr);
+			String tmps = null;
+			int start = -1;
+			int end = -1;
+			int startz = -1;
+			while ((tmps = br.readLine()) != null) {
+				if (startz >= 0) {
+					sb.append(tmps);
+				} else {
+					// 开始找title
+					start = tmps.indexOf("<title>");
+					end = tmps.indexOf("/title>");
+					// System.out.println(tmps);
+					if (start >= 0) {
+						// 找到
+						if (end >= 0) {
+							return tmps.substring(start + 7, end - 1);// 防止乱码
+						}
+						sb.append(tmps);
+						startz = start;
+						continue;
+					}// 没找到
+					continue;
+				}
+				// 找 /title
+				end = sb.indexOf("/title>");
+				if (end >= 0) {// 找到
+					startz = sb.indexOf("<title>") + 7;
+					return sb.substring(startz, end - 1);
+				}
+				// 没找到
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return "";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getTitle("http://news.163.com/14/1211/18/AD746G3F00014JB5.html"));
+	}
 }
